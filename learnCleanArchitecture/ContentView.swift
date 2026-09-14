a799df9d6464bbfd0d9a18de7cbed2e47543fe72
Maksim <maksim.minakov.83@mail.ru>
2026-09-14 22:29:46 +0300

    Initial Commit

---
+++ learnCleanArchitecture/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  learnCleanArchitecture
+//
+//  Created by Максим Минаков on 14.09.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ learnCleanArchitecture/learnCleanArchitectureApp.swift
@@ -0,0 +1,17 @@
+//
+//  learnCleanArchitectureApp.swift
+//  learnCleanArchitecture
+//
+//  Created by Максим Минаков on 14.09.2026.
+//
+
+import SwiftUI
+
+@main
+struct learnCleanArchitectureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
